565322ef8bd4fba4cd92cd2d5342c52705c8676c
James Cai <jamescai@genomezoo.net>
2021-01-13 18:23:48 -0600

    Update ScTenifold.jl

--- src/ScTenifold.jl
+++ src/ScTenifold.jl
@@ -4,6 +4,7 @@ export Net, Knk, sctenifoldnet, sctenifoldknk, sctenifoldxct
 include("sctenifoldnet.jl")
 include("sctenifoldknk.jl")
 include("sctenifoldxct.jl")
+include("support.jl")
 
 using .Net
 using .Knk
